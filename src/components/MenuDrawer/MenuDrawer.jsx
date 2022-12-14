a5126e132050e21da5fcd1f6f32028558a97cdb4
PriyanshuPatil <pantuugurjar@gmail.com>
2022-12-14 21:37:33 +0530

    added menu-drawer

---
+++ src/components/MenuDrawer/MenuDrawer.jsx
@@ -0,0 +1,30 @@
+import { Button, Drawer, DrawerBody, DrawerContent, DrawerHeader, DrawerOverlay, Radio, RadioGroup, Stack, useDisclosure } from "@chakra-ui/react"
+import React from "react"
+export function MenuDrawer() {
+    const { isOpen, onOpen, onClose } = useDisclosure()
+    const [placement, setPlacement] = React.useState('left')
+  
+    return (
+      <>
+        <RadioGroup defaultValue={placement} onChange={setPlacement}>
+          <Stack direction='row' mb='1'>
+            <Radio value='left'>Left</Radio>
+          </Stack>
+        </RadioGroup>
+        <Button colorScheme='blue' onClick={onOpen}>
+          Open
+        </Button>
+        <Drawer placement={placement} onClose={onClose} isOpen={isOpen}>
+          <DrawerOverlay />
+          <DrawerContent>
+            <DrawerHeader borderBottomWidth='1px'>Basic Drawer</DrawerHeader>
+            <DrawerBody>
+              <p>Some contents...</p>
+              <p>Some contents...</p>
+              <p>Some contents...</p>
+            </DrawerBody>
+          </DrawerContent>
+        </Drawer>
+      </>
+    )
+  }
